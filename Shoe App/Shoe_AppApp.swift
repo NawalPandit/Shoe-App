bd6271d554a3a3cc5947657d5c220fc60b7a4e0a
NawalPandit <nawal.pt19@gmail.com>
2023-11-21 15:04:58 -0600

    Initial Commit

---
+++ Shoe App/Shoe_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shoe_AppApp.swift
+//  Shoe App
+//
+//  Created by Nawal Pandit on 8/27/23.
+//
+
+import SwiftUI
+
+@main
+struct Shoe_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
